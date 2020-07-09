284eec009b1d2d24e5c45d59604318f10d90fd7d
Marius Guede <marius.guede@sleeksys.com>
2020-07-09 17:48:51 +0200

    Spring Boot: add system files

---
+++ spring-boot/src/main/java/com/sleeksys/app/anonymizer/AnonymizerApplication.java
@@ -0,0 +1,12 @@
+package com.sleeksys.app.anonymizer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnonymizerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AnonymizerApplication.class, args);
+    }
+}
---
+++ spring-boot/src/test/java/com/sleeksys/app/anonymizer/AnonymizerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sleeksys.app.anonymizer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnonymizerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
