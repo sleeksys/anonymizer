77a88d952c7fd046abb8ac8c6fde0bec17a8520a
Marius Guede <marius.guede@sleeksys.com>
2020-07-10 08:33:37 +0200

    Spring Boot: create tables

---
+++ spring-boot/create.sql
@@ -0,0 +1,6 @@
+create sequence hibernate_sequence start with 1 increment by 1
+create table labels (id bigint not null, privacy_level integer, text varchar(255), primary key (id))
+create sequence hibernate_sequence start with 1 increment by 1
+create table labels (id bigint not null, privacy_level integer, text varchar(255), primary key (id))
+create sequence hibernate_sequence start with 1 increment by 1
+create table label (id bigint not null, privacy_level integer, text varchar(255), primary key (id))
